a87f4b572ea3cad28a9413e468af02127685377a
krouhifar-vn <krouhifar@viralnation.com>
2023-01-15 23:38:45 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Box, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Box className="App">
+      <Typography variant="h4">That is the only beginning.</Typography>
+    </Box>
+  );
+}
+
+export default App;
